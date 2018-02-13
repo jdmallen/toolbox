ac3967156f70fd51c83b4d2546097e3475779f9e
Jesse Mallen <lightandtheheat@gmail.com>
2018-02-12 23:27:31 -0500

    Begin work on services arch

---
+++ JDMallen.Toolbox.Microservices/Models/IService.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JDMallen.Toolbox.Models;
+using JDMallen.Toolbox.RepositoryPattern.Interfaces;
+
+namespace JDMallen.Toolbox.Microservices.Models
+{
+	public interface IService
+	{
+	}
+
+	public interface IService<out TRepository, TDomainModel, in TQueryParameters, in TId>
+		where TRepository : IRepository
+		where TDomainModel : IModel
+		where TQueryParameters : IQueryParameters
+		where TId : struct
+	{
+		TRepository Repository { get; }
+
+		#region C
+
+		Task<TDomainModel> Create(TDomainModel model);
+
+		#endregion
+
+		#region R
+		Task<TDomainModel> Read(TId id);
+
+		Task<TDomainModel> Find(TQueryParameters parameters);
+
+		Task<IEnumerable<TDomainModel>> FindAll(TQueryParameters parameters);
+
+		Task<IPagedResult<TDomainModel>> FindAllPaged(TQueryParameters parameters);
+
+		#endregion
+
+		#region U
+
+		Task<TDomainModel> Update(TDomainModel model);
+
+		#endregion
+
+		#region D
+
+		Task<TDomainModel> Delete(TDomainModel model);
+
+		Task<TDomainModel> Delete(TId id);
+
+		#endregion
+	}
+}
---
+++ JDMallen.Toolbox.Microservices/Models/Service.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JDMallen.Toolbox.Models;
+using JDMallen.Toolbox.RepositoryPattern.Interfaces;
+
+namespace JDMallen.Toolbox.Microservices.Models
+{
+	public abstract class Service<TRepository, TDomainModel, TQueryParameters, TId>
+		: IService<TRepository, TDomainModel, TQueryParameters, TId>
+		where TRepository : IRepository
+		where TDomainModel : IModel
+		where TQueryParameters : IQueryParameters
+		where TId : struct
+	{
+		protected Service(TRepository repository)
+		{
+			Repository = repository;
+		}
+
+		public TRepository Repository { get; }
+
+		public abstract Task<TDomainModel> Create(TDomainModel model);
+
+		public abstract Task<TDomainModel> Read(TId id);
+
+		public abstract Task<TDomainModel> Find(TQueryParameters parameters);
+
+		public abstract Task<IEnumerable<TDomainModel>> FindAll(TQueryParameters parameters);
+
+		public abstract Task<IPagedResult<TDomainModel>> FindAllPaged(TQueryParameters parameters);
+
+		public abstract Task<TDomainModel> Update(TDomainModel model);
+
+		public abstract Task<TDomainModel> Delete(TDomainModel model);
+
+		public abstract Task<TDomainModel> Delete(TId id);
+	}
+}
